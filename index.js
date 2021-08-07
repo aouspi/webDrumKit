537f90a745c222a4a6db6b63694a0093a65e1c8a
aouspi <69452118+aouspi@users.noreply.github.com>
2021-08-07 21:12:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+
+var numberOfDrums = document.querySelectorAll(".drum").length;
+
+for(var i=0;i<numberOfDrums;i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click",function (){
+      var buttonInnerHtml = this.innerHTML;
+      getSound(buttonInnerHtml);
+      activateAnimation(buttonInnerHtml);
+  });
+}
+
+document.addEventListener("keydown",function(event){
+  getSound(event.key);
+  activateAnimation(event.key);
+
+});
+
+function activateAnimation(key){
+  var anime = document.querySelector("."+key);
+  anime.classList.add("pressed");
+  setTimeout(function () {
+    anime.classList.remove("pressed");
+  }, 100);
+}
+
+function getSound(key){
+  switch(key) {
+    case "w":
+        var crash = new Audio("crash.mp3");
+        crash.play();
+        break;
+    case "a":
+        var kick = new Audio("kick-bass.mp3");
+        kick.play();
+      break;
+   case "s":
+        var snare = new Audio("snare.mp3");
+        snare.play();
+        break;
+   case "d":
+        var tom1 = new Audio("tom-1.mp3");
+        tom1.play();
+        break;
+   case "j":
+        var tom2 = new Audio("tom-2.mp3");
+        tom2.play();
+        break;
+   case "k":
+        var tom3 = new Audio("tom-3.mp3");
+        tom3.play();
+        break;
+   case "l":
+        var tom4 = new Audio("tom-4.mp3");
+        tom4.play();
+        break;
+    default:
+    console.log(buttonInnerHtml);
+
+  }
+}
